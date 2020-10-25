791bb981c6a26efc4cb08ba9c1def0925a2de1ca
mapmo <mapmoeu@gmail.com>
2020-10-25 16:29:12 +0200

    Change the script's behavior based on its first atgument. If the argument is a .zip file, it will attempt to unzip it first. If the first argument is a directory, the script will assume that the script has already been used to unzip and compile the files and will proceed to testing. User is able to pass which students to be tested specifically by typing their number as cli arguments. User is able to also pass --noxec option which will cause the script to only build the files

--- scripts/cpp_homework_tester/test_homeworks.py
+++ scripts/cpp_homework_tester/test_homeworks.py
@@ -13,20 +13,22 @@ def create_tasks_list(tests_dir):
     return tasks_test_dirs
 
 
-def test_homeworks(tests_dir, tasks_test_dirs):
+def test_homeworks(students_to_test, tasks_test_dirs):
     all_scores_fd = open(".results", "w+")
-    for student_dir in glob.glob("*"):
+    print("Tasks to test", students_to_test)
+    for student_dir in students_to_test:
         student_scores = list()
         os.chdir(student_dir)
         for task_test_dir in tasks_test_dirs:
             task_number = os.path.basename(task_test_dir)
             task_score = 0
             for task_test in glob.glob(os.path.join(task_test_dir, "*-in")):
-                task_solution = task_test.replace("-in", "-out")
-                tmpfile = "tmpfile"
                 list_student_file = glob.glob("*_" + task_number + "_*.exe")
                 if len(list_student_file) == 0:
-                    continue
+                    break
+
+                task_solution = task_test.replace("-in", "-out")
+                tmpfile = "tmpfile"
                 student_file = os.path.join(".", list_student_file[0])
                 command = "cat " + task_test + " | timeout 2 " + student_file + " > " + tmpfile
                 print(command)
@@ -40,11 +42,16 @@ def test_homeworks(tests_dir, tasks_test_dirs):
     all_scores_fd.close()
 
 
-def main(tests_dir):
+def main():
+    tests_dir = sys.argv[2]
     tasks_test_dirs = create_tasks_list(tests_dir)
-    test_homeworks(tests_dir, tasks_test_dirs)
+
+    if len(sys.argv) > 3:
+        students_to_test = sys.argv[3:]
+    else:
+        students_to_test = glob.glob("*")
+    test_homeworks(students_to_test, tasks_test_dirs)
 
 
 if __name__ == "__main__":
-    tests_dir = sys.argv[1]
-    main(tests_dir)
+    main()
