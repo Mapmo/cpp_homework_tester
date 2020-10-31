71139df4d5182c3a6e0cb2017677da8558033fd1
mapmo <mapmoeu@gmail.com>
2020-11-01 01:20:12 +0200

    Refactor the execute_test function so that it no longer handles the task_score variable. Due to being a primitive type, its value was not passed by reference to the function and this way the original task_score was not changed. The new solution uses less lines of code as well, since it no longer has an else statement

--- scripts/cpp_homework_tester/test_homeworks.py
+++ scripts/cpp_homework_tester/test_homeworks.py
@@ -14,31 +14,28 @@ def create_tasks_list(tests_dir):
     return tasks_test_dirs
 
 
-def execute_test(task_test, list_student_task_solution, task_score):
-    test = dict()
+def execute_test(task_test, list_student_task_solution):
     test_solution = task_test.replace("-in", "-out")
     tmpfile = "tmpfile"
-
     student_task_solution = os.path.join(".", list_student_task_solution)
+
     command = "echo $(cat " + task_test + " | timeout 2 " + student_task_solution + " | tr [a-z] [A-Z]) > " + tmpfile
     print(command)
     os.system(command)
 
     test_solution_fd = open(test_solution)
     tmpfile_fd = open(tmpfile)
+
+    test = dict()
     test["id"] = os.path.basename(task_test)[:-3]
     test["expect_result"] = test_solution_fd.read()
     test["actual_result"] = tmpfile_fd.read()
+    test["match"] = filecmp.cmp(test_solution, tmpfile)
+
     test_solution_fd.close()
     tmpfile_fd.close()
-
-    if filecmp.cmp(test_solution, tmpfile):
-        task_score += 1
-        test["match"] = 1
-    else:
-        test["match"] = 0
-
     os.unlink(tmpfile)
+
     return test
 
 
@@ -68,8 +65,10 @@ def test_homeworks(students_to_test, tasks_test_dirs):
             student_task["id"] = task_number
             student_task["tests"] = list()
             for task_test in glob.glob(os.path.join(task_test_dir, "*-in")):
-                test = execute_test(task_test, list_student_task_solution[0], task_score)
+                test = execute_test(task_test, list_student_task_solution[0])
                 student_task["tests"].append(test)
+                if test["match"]:
+                    task_score += 1
             student_scores.append(task_score)
             student_tasks.append(student_task)
         append_student_result(data, student_dir, student_scores, student_tasks)
