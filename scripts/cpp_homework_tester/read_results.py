6a68582df3b8edcece32750cc40fff26ff90d23d
mapmo <mapmoeu@gmail.com>
2020-11-02 00:36:05 +0200

    Refactor the code in read_results.py in order to make it cleaner and easier for reading and understanding

--- scripts/cpp_homework_tester/read_results.py
+++ scripts/cpp_homework_tester/read_results.py
@@ -20,6 +20,30 @@ def parse_json_file(json_file):
     return results
 
 
+def extract_student_result(results, faculty_number):
+    for student_result in results:
+        if student_result["faculty_number"] == faculty_number:
+            return student_result
+    print("No student with faculty number", faculty_number, "found")
+    exit(2)
+
+
+def extract_student_task(student_result, task_id):
+    for student_task in student_result["tasks"]:
+        if student_task["id"] == task_id:
+            return student_task
+    print("No task with id", task_id, "found in the student's results")
+    exit(3)
+
+
+def extract_student_test(student_task, test_id):
+    for student_test in student_task["tests"]:
+        if student_test["id"] == test_id:
+            return student_test
+    print("No test with id", test_id, "found in the student's tasks")
+    exit(4)
+
+
 argc = len(sys.argv)
 if argc < 2 or argc > 5:
     print("Usage:", sys.argv[0], "JSON_FILE FN TASK_ID TEST_ID")
@@ -27,49 +51,21 @@ if argc < 2 or argc > 5:
 
 json_file = (sys.argv[1])
 results = parse_json_file(json_file)
-
 if argc == 2:
     print(json.dumps(results, indent=4))
     exit(0)
 
 faculty_number = sys.argv[2]
-student_matched = False
-for student_result in results:
-    if student_result["faculty_number"] == faculty_number:
-        student_matched = True
-        break
-
-if not student_matched:
-    print("No student with faculty number", faculty_number, "found")
-    exit(2)
-
+student_result = extract_student_result(results, faculty_number)
 if argc == 3:
     print(json.dumps(student_result, indent=4))
     exit(0)
 
 task_id = sys.argv[3]
-task_matched = False
-for student_task in student_result["tasks"]:
-    if student_task["id"] == task_id:
-        task_matched = True
-        break
-
-if not task_matched:
-    print("No task with id", task_id, "found in the student's results")
-    exit(3)
-
+student_task = extract_student_task(student_result, task_id)
 if argc == 4:
     print(json.dumps(student_task, indent=4))
 
 test_id = sys.argv[4]
-test_matched = False
-for student_test in student_task["tests"]:
-    if student_test["id"] == test_id:
-        test_matched = True
-        break
-
-if not test_matched:
-    print("No test with id", test_id, "found in the student's tasks")
-    exit(4)
-
+student_test = extract_student_test(student_task, test_id)
 print(json.dumps(student_test, indent=4))
