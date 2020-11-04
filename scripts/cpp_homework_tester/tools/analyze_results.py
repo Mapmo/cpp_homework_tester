c5f5712c3903d6a0ff5d3d696897a5552215eb9d
mapmo <mapmoeu@gmail.com>
2020-11-04 23:03:46 +0200

    Extract the functionality to print average score for all tasks and for all tests of each task in separate functions. Also made the program print only them when no additional parameters are passed

--- scripts/cpp_homework_tester/tools/analyze_results.py
+++ scripts/cpp_homework_tester/tools/analyze_results.py
@@ -4,9 +4,41 @@ from libs import tools_functions
 import numpy as np
 import sys
 
+
+def calculate_average_tasks(all_results):
+    all_results = np.array(all_results)
+    all_results_average = list(map(lambda x: round(x, 2), np.average(all_results, axis=0)))
+    print("The average score for each task is:", all_results_average)
+    return all_results_average
+
+
+def calculate_average_tasks_tests(tasks):
+    average_task_tests = dict()
+    for task_id in range(1, tasks + 1):
+        current_task_tests = list()
+        for student in results:
+            for task in student["tasks"]:
+                if task["id"] == str(task_id):
+                    student_task_tests = list()
+                    for test in task["tests"]:
+                        try:
+                            student_task_tests.append(int(test["match"]))
+                        except TypeError:
+                            break
+                    if len(student_task_tests) > 0:
+                        current_task_tests.append(student_task_tests)
+                    break
+        current_task_tests = np.array(current_task_tests)
+        average_current_task_tests = list(map(lambda x: round(x, 2), np.average(current_task_tests, axis=0)))
+        average_task_tests[task_id] = average_current_task_tests
+    for task_id in average_task_tests.keys():
+        print("The average score for the tests of each Task #" + str(task_id) + " is: ", average_task_tests[task_id])
+    return average_task_tests
+
+
 argc = len(sys.argv)
-if argc > 2:
-    print("Usage:", sys.argv[0], "JSON_FILE")
+if argc < 2 or argc > 5:
+    print("Usage:", sys.argv[0], "JSON_FILE [TASK] [TEST_ID]")
     exit(1)
 
 json_file = (sys.argv[1])
@@ -15,35 +47,15 @@ results = tools_functions.parse_json_file(json_file)
 all_results = list()
 for student in results:
     all_results.append(student["score"])
-
 if len(all_results) <= 0:
     print("Critical error, no results extracted from", json_file)
 
-all_results = np.array(all_results)
-all_results_average = list(map(lambda x: round(x, 2), np.average(all_results, axis=0)))
-print("The average score for each task is:", all_results_average)
+all_results_average = calculate_average_tasks(all_results)
+average_task_tests = calculate_average_tasks_tests(len(all_results_average))
 
-average_task_tests = dict()
-for task_id in range(1, len(all_results_average) + 1):
-    current_task_tests = list()
-    for student in results:
-        for task in student["tasks"]:
-            if task["id"] == str(task_id):
-                student_task_tests = list()
-                for test in task["tests"]:
-                    try:
-                        student_task_tests.append(int(test["match"]))
-                    except TypeError:
-                        break
-                if len(student_task_tests) > 0:
-                    current_task_tests.append(student_task_tests)
-                break
-    current_task_tests = np.array(current_task_tests)
-    average_current_task_tests = list(map(lambda x: round(x, 2), np.average(current_task_tests, axis=0)))
-    average_task_tests[task_id] = average_current_task_tests
+if argc == 2:
+    exit(0)
 
-for task_id in average_task_tests.keys():
-    print("The average result for the tests of each Task #" + str(task_id) + " is: ", average_task_tests[task_id])
 for task_id in range(1, len(all_results_average) + 1):
     found_task = False
     for student in results:
