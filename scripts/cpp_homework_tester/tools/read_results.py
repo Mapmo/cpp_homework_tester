b95d3d698763a7b2622a69283571af2a4600be7b
mapmo <mapmoeu@gmail.com>
2020-11-14 14:26:15 +0200

    Improve script for reading results to have an option for printing only the tests that have failed

--- scripts/cpp_homework_tester/tools/read_results.py
+++ scripts/cpp_homework_tester/tools/read_results.py
@@ -4,13 +4,40 @@ from libs import tools_functions
 import json
 import sys
 
+
+false_option = "--false"
+
+
+def check_for_false_option():
+    if false_option in sys.argv:
+        sys.argv.remove(false_option)
+        return True
+    return False
+
+
+def remove_successful_tests():
+    for student in results:
+        for task in student["tasks"]:
+            if isinstance(task["tests"][0], str):
+                continue
+            for test in range(len(task["tests"]), 0, -1):
+                if task["tests"][test - 1]["match"]:
+                    del task["tests"][test - 1]
+
+
+false_option_set = check_for_false_option()
 argc = len(sys.argv)
 if argc < 2 or argc > 5:
-    print("Usage:", sys.argv[0], "JSON_FILE FN TASK_ID TEST_ID")
+    print("Usage:", sys.argv[0], "JSON_FILE [[[FN] [TASK_ID] [TEST_ID]] [" + false_option + "]]")
     exit(1)
+
 
 json_file = (sys.argv[1])
 results = tools_functions.parse_json_file(json_file)
+
+if false_option_set:
+    remove_successful_tests()
+
 if argc == 2:
     print(json.dumps(results, indent=4))
     exit(0)
