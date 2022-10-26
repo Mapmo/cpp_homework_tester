c5e7e6e61fb02255a188953d36e0ea6164b5a3b6
Martin Iliev <mapmoeu@gmail.com>
2022-10-27 00:39:26 +0300

    Update tools/libs/tools_functions.py style

--- tools/libs/tools_functions.py
+++ tools/libs/tools_functions.py
@@ -7,13 +7,13 @@ from termcolor import colored
 
 def parse_json_file(json_file):
     if not os.path.isfile(json_file):
-        print(json_file, "is not a regular file")
+        print(f"{json_file} is not a regular file")
         exit(1)
     try:
         with open(json_file) as json_file_fd:
             results = json.load(json_file_fd)
     except ValueError:
-        print(json_file, "not a valid json file")
+        print(f"{json_file} not a valid json file")
         exit(1)
     return results
 
@@ -22,7 +22,7 @@ def extract_student_result(results, faculty_number):
     for student_result in results:
         if student_result["faculty_number"] == faculty_number:
             return student_result
-    print("No student with faculty number", faculty_number, "found")
+    print(f"No student with faculty number {faculty_number} found")
     exit(2)
 
 
@@ -30,7 +30,7 @@ def extract_student_task(student_result, task_id):
     for student_task in student_result["tasks"]:
         if student_task["id"] == task_id:
             return student_task
-    print("No task with id", task_id, "found in the student's results")
+    print(f"No task with id {task_id} found in the student's results")
     exit(3)
 
 
@@ -38,8 +38,11 @@ def extract_student_test(student_task, test_id):
     for student_test in student_task["tests"]:
         if student_test["id"] == test_id:
             return student_test
-    print("No test with id", test_id, "found in the student's tasks. Maybe using '--false' unintentionally?")
+    print(
+        f"No test with id {test_id} found in the student's tasks. Maybe using '--false' unintentionally?"
+    )
     exit(4)
+
 
 # Used when the HW has 3 tasks and each task earns 3 points
 def grade_3(score):
@@ -87,6 +90,7 @@ def grade_5(score):
     else:
         final = 0.0
     return final
+
 
 def pick_color(final):
     if final > 2:
