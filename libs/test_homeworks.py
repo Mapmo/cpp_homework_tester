78d5044ac5b5c50985b4a6d36e65a6c1f555251a
Martin Iliev <mapmoeu@gmail.com>
2022-10-26 23:54:32 +0300

    Update style of libs/test_homeworks.py

--- libs/test_homeworks.py
+++ libs/test_homeworks.py
@@ -1,12 +1,10 @@
-#!/bin/env python3
-
 import glob
 import json
 import os
 import re
 import sys
 
-execute_dir = ''
+execute_dir = ""
 
 
 def atoi(text):
@@ -14,17 +12,19 @@ def atoi(text):
 
 
 def natural_keys(text):
-    return list(atoi(x) for x in re.split(r'(\d+)', text))
+    return list(atoi(x) for x in re.split(r"(\d+)", text))
 
 
 def create_tasks_list(tests_dir):
     cwd = os.getcwd()
-    os.chdir(tests_dir)  # used when the zip file for build_solutions is in a separate directory and user uses relative paths
-    tasks_test_dirs = list()
-    for task_test_dir in glob.glob('*'):
-        tasks_test_dirs.append(os.path.join(tests_dir, task_test_dir))
+    os.chdir(
+        tests_dir
+    )  # used when the zip file for build_solutions is in a separate directory and user uses relative paths
+    tasks = list()
+    for task_dir in glob.glob("*"):
+        tasks.append(os.path.join(tests_dir, task_dir))
     os.chdir(cwd)
-    return sorted(tasks_test_dirs, key=natural_keys)
+    return sorted(tasks, key=natural_keys)
 
 
 def execute_test(test_input, list_student_task_solution):
@@ -32,7 +32,7 @@ def execute_test(test_input, list_student_task_solution):
     student_test_output = "/tmp/.tmpfile"
     student_task_solution = re.escape(os.path.join(".", list_student_task_solution))
 
-    command = "cat " + test_input + " | timeout 1 " + student_task_solution + " | head -c 300 > " + student_test_output
+    command = f"cat {test_input} | timeout 1 {student_task_solution} | head -c 300 > {student_test_output}"
     os.system(command)
 
     with open(test_input) as test_input_fd:
@@ -43,9 +43,11 @@ def execute_test(test_input, list_student_task_solution):
                 test["input"] = test_input_fd.read()
                 test["expect_output"] = expected_test_output_fd.read().strip().lower()
                 try:
-                    test["actual_output"] = student_test_output_fd.read().strip().lower()
+                    test["actual_output"] = (
+                        student_test_output_fd.read().strip().lower()
+                    )
                 except UnicodeDecodeError:
-                    print("File", student_task_solution, "produces some unicode issues")
+                    print(f"File {student_task_solution} produces some unicode issues")
                     test["actual_output"] = "encoding issue"
                 test["match"] = test["expect_output"] == test["actual_output"]
 
@@ -54,11 +56,9 @@ def execute_test(test_input, list_student_task_solution):
 
 
 def append_student_result(data, student_dir, student_scores, student_tasks):
-    data.append({
-        "faculty_number": student_dir,
-        "score": student_scores,
-        "tasks": student_tasks
-    })
+    data.append(
+        {"faculty_number": student_dir, "score": student_scores, "tasks": student_tasks}
+    )
 
 
 def dump_results_to_file(data):
@@ -66,28 +66,30 @@ def dump_results_to_file(data):
         json.dump(data, results_file_fd)
 
 
-def test_homeworks(students_to_test, tasks_test_dirs):
+def test_homeworks(students_to_test, tasks):
     data = list()
     for student_dir in students_to_test:
         student_score = list()
         student_tasks = list()
         os.chdir(student_dir)
-        for task_test_dir in tasks_test_dirs:
-            task_number = os.path.basename(task_test_dir)
-            list_student_task_solution = glob.glob("*_" + task_number + "_*.exe")
+        for task_id in tasks:
+            task_number = os.path.basename(task_id)
+            student_task_solution = glob.glob(f"*_{task_number}_*.exe")
             student_task_score = 0
             student_task = dict()
             student_task["id"] = task_number
             student_task["tests"] = list()
-            if len(list_student_task_solution) == 0:
+            if len(student_task_solution) == 0:
                 test = "Compiled solution not found"
                 student_task["tests"].append(test)
             else:
-                task_tests = sorted(glob.glob(os.path.join(task_test_dir, "*-in")), key=natural_keys)
-                for task_test in task_tests:
-                    test = execute_test(task_test, list_student_task_solution[0])
-                    student_task["tests"].append(test)
-                    student_task_score += test["match"]
+                task_tests = sorted(
+                    glob.glob(os.path.join(task_id, "*-in")), key=natural_keys
+                )
+                for test in task_tests:
+                    test_result = execute_test(test, student_task_solution[0])
+                    student_task["tests"].append(test_result)
+                    student_task_score += test_result["match"]
                 student_task_score = round(student_task_score / len(task_tests), 2)
             student_score.append(student_task_score)
             student_tasks.append(student_task)
@@ -98,12 +100,12 @@ def test_homeworks(students_to_test, tasks_test_dirs):
 
 def main():
     tests_dir = os.path.join(execute_dir, sys.argv[2])
-    tasks_test_dirs = create_tasks_list(tests_dir)
+    tasks = create_tasks_list(tests_dir)
     if len(sys.argv) > 3:
         students_to_test = sys.argv[3:]
     else:
         students_to_test = sorted(glob.glob("*"))
-    test_homeworks(students_to_test, tasks_test_dirs)
+    test_homeworks(students_to_test, tasks)
 
 
 if __name__ == "__main__":
